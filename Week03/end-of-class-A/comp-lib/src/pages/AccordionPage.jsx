5f30ddbd1822df1e73ec89cbb19ee7dbb2fdceed
Katie <kaa328@nyu.edu>
2026-09-15 09:27:26 -0400

    Add completed button component with variants

---
+++ Week03/end-of-class-A/comp-lib/src/pages/AccordionPage.jsx
@@ -0,0 +1,12 @@
+import Accordion from '../components/Accordion'
+
+const AccordionPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">AccordionPage</h1>
+      <Accordion />
+    </div>
+  )
+}
+
+export default AccordionPage
